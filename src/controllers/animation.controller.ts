5bfc2dbae5079c6ab4737da51306cab6c94febce
LAPTOP-E09VQ1LE\Anton <calerolozanoantonio@gmail.com>
2020-12-22 09:50:56 +0100

    Añadidos los gards

--- src/controllers/animation.controller.ts
+++ src/controllers/animation.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 import { ObjectId, ObjectID } from "mongodb";
 import { AnimationService } from "src/services/animation.service";
 
@@ -10,12 +11,14 @@ export class AnimationController {
     }
 
     @Get()
+    @UseGuards(AuthGuard('jwt'))
     public async findAll(): Promise<Array<any>> {
 
         return await this.service.getAll();
     }
 
     @Get()
+    @UseGuards(AuthGuard('jwt'))
     public async find(@Param('id') id: number): Promise<any> {
 
         return this.service.get(new ObjectId(id));
--- src/controllers/fighter.controller.ts
+++ src/controllers/fighter.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 import { Fighter } from "src/models/entities/fighter.entity";
 import { FighterService } from "src/services/fighter.service";
 
@@ -11,36 +12,42 @@ export class FighterController {
     }
 
     @Get()
+    @UseGuards(AuthGuard('jwt'))
     public getAll() {
 
         return this.service.getAll();
     }
 
     @Get(':id')
+    @UseGuards(AuthGuard('jwt'))
     public async getById(@Param('id', ParseIntPipe) id: number): Promise<Fighter> {        
 
         return this.service.getById(id);
     }
 
     @Get(':name')
+    @UseGuards(AuthGuard('jwt'))
     public getByName(@Param('name') name: string) {
 
         return this.service.getByName(name);
     }
 
     @Post()
+    @UseGuards(AuthGuard('jwt'))
     public save(@Body() fighter: Fighter) {
 
         return this.service.save(fighter);
     }
 
     @Put()
+    @UseGuards(AuthGuard('jwt'))
     public async update(@Param('id', ParseIntPipe) id: number, @Body() fighter: Fighter) {
 
         return await this.service.update(id, fighter);
     }
 
     @Delete()
+    @UseGuards(AuthGuard('jwt'))
     public async delete(@Param('id', ParseIntPipe) id: number) {
 
         return this.service.removeById(id);
